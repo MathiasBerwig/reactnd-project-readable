c1fa8521693bc1ba7d1095690e9a59a344606ae5
Mathias Berwig <mathiasberwig@gmail.com>
2019-02-26 17:26:31 -0300

    Add Header with category name on Feed

--- frontend/src/components/Feed.jsx
+++ frontend/src/components/Feed.jsx
@@ -2,7 +2,7 @@
 
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
-import { List, Container } from 'semantic-ui-react';
+import { List, Container, Header } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
 import { PropTypes } from 'prop-types';
 import { handleReceivePosts } from '../actions/posts';
@@ -25,11 +25,11 @@ class Feed extends PureComponent {
 
   onRouteChanged() {
     const { dispatch } = this.props;
-    const category = this.getCurrentCategory();
+    const category = this.getCurrentCategoryFromPath();
     dispatch(handleReceivePosts(category));
   }
 
-  getCurrentCategory() {
+  getCurrentCategoryFromPath() {
     const { location: { pathname } } = this.props;
     return pathname.substr(1);
   }
@@ -40,6 +40,7 @@ class Feed extends PureComponent {
 
     return (
       <Container style={{ paddingTop: '7em' }}>
+        <Header as="h1">{this.getCurrentCategoryFromPath() || 'All Posts'}</Header>
         {
           orderedPosts === undefined || orderedPosts.length === 0
             ? <FeedEmpty />
@@ -72,8 +73,9 @@ Feed.defaultProps = {
   orderBy: 'score',
 };
 
-function mapStateToProps({ posts, preferences = {} }) {
+function mapStateToProps({ categories, posts, preferences = {} }) {
   return {
+    categories,
     orderBy: preferences.orderBy,
     posts: Object.values(posts),
   };
